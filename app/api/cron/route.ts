c33fc96637cf5a54a1a158ae3227412e29fde0a5
acu8 <ewatanabe786@gmail.com>
2024-10-14 15:26:29 +0900

    Fix: 環境変数修正

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,8 +7,8 @@ import { createClient } from '@supabase/supabase-js'
 export const runtime = 'edge';
 
 const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!
   )
 
 async function fetchAndProcessBooks(): Promise<BookCount[]> {
